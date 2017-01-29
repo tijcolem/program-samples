4db3a7ab9ed3aa585421c04cffd084f4e81f1a4e
tim <tim@tim-Inspiron-13-7359>
2017-01-29 10:04:32 -0700

    commited

--- DataDistrubution/SendRealTimeDataToGSD.py
+++ DataDistrubution/SendRealTimeDataToGSD.py
@@ -37,7 +37,7 @@ def calday(i, hr, day):  # function to calc days
 
 
 PutPath = '/pub/psd'  # FTP put directory
-BasePath = '/data  # define source base path
+BasePath = '/data'  # define source base path. Some thing
 
 FTPServer = 'gpsdmz.fsl.noaa.gov'
 User = '***'
